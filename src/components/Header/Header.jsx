bc1e11dcf1de06f323705277dfe6afbc3c6583a1
Aarati <aaratishah10@gmail.com>
2021-05-04 23:53:43 +0530

    added a new .svg file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css';
+import Logo from './logo.svg';
 
 
 
@@ -7,7 +8,7 @@ import './Header.css';
 const Header = ({ headTitle, headerExpanded }) => {
     return (
         <div className="head-container">
-            <img src="https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png"
+            <img src={Logo}
                 className={`head-image ${headerExpanded ? 'head-image-expanded' : 'head-image-contracted'}`}
                 alt="headerImage">
 
